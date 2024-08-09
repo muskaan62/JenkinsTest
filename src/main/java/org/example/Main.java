280248a6f7b93c034cbf6665ed0fee8624582707
muskaan62 <muskaanbegum23@gmail.com>
2024-08-09 16:30:42 +0530

    Test Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Learning jenkins");
+        System.out.println("Hello world!");
+    }
+}
